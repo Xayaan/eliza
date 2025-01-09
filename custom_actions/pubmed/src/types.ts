3016b10f7ad352a557c5afbef0fbc6b08989c817
Zayaan <x_ayan@hotmail.com>
2025-01-09 19:21:09 +0530

    Update types.ts

--- custom_actions/pubmed/src/types.ts
+++ custom_actions/pubmed/src/types.ts
@@ -3,7 +3,22 @@ import { PubmedConfigSchema, PubmedSearchParamsSchema } from './schema';
 
 export type PubMedConfig = z.infer<typeof PubmedConfigSchema>;
 export type PubMedSearchParams = z.infer<typeof PubmedSearchParamsSchema>;
+import { ActionResponse } from '@elizaos/core';
 
+export interface PubMedActionInput {
+  query?: string;
+  maxResults?: number;
+}
+
+export interface PubMedActionOutput extends ActionResponse {
+  articles: Array<{
+    id: string;
+    title: string;
+    abstract: string;
+    url: string;
+    publishDate: string;
+  }>;
+}
 export interface PubMedArticle {
   id: string;
   title: string;
