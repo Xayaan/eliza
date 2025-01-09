bd7153f470f130910600e9ffbe647e30b802e02b
Zayaan <x_ayan@hotmail.com>
2025-01-09 20:28:00 +0530

    Update pubmed.ts

--- custom_actions/pubmed/src/utils/pubmed.ts
+++ custom_actions/pubmed/src/utils/pubmed.ts
@@ -1,26 +1,64 @@
-// src/utils/pubmed.ts (continued)
-export async function searchPubMed(query: string): Promise<PubMedArticle[]> {
-  const baseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
-  const searchUrl = `${baseUrl}/esearch.fcgi?db=pubmed&term=${encodeURIComponent(query)}&retmode=json&retmax=5`;
-  
-  const searchResponse = await fetch(searchUrl);
-  const searchData = await searchResponse.json();
-  const ids = searchData.esearchresult.idlist;
+import axios from 'axios';
+import { PubMedArticle, PubMedSearchResponse } from './types';
 
-  if (!ids.length) return [];
+const PUBMED_API = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
 
-  const summaryUrl = `${baseUrl}/esummary.fcgi?db=pubmed&id=${ids.join(',')}&retmode=json`;
-  const summaryResponse = await fetch(summaryUrl);
-  const summaryData = await summaryResponse.json();
+export async function searchPubMed(query: string, maxResults: number): Promise<PubMedSearchResponse> {
+  try {
+    // Search for IDs first
+    const searchResponse = await axios.get(`${PUBMED_API}/esearch.fcgi`, {
+      params: {
+        db: 'pubmed',
+        term: query,
+        retmax: maxResults,
+        retmode: 'json',
+        usehistory: 'y'
+      }
+    });
 
-  return ids.map(id => {
-    const article = summaryData.result[id];
+    const ids = searchResponse.data.esearchresult.idlist;
+
+    if (!ids.length) {
+      return {
+        articles: [],
+        total: 0,
+        query
+      };
+    }
+
+    // Fetch article details
+    const detailsResponse = await axios.get(`${PUBMED_API}/esummary.fcgi`, {
+      params: {
+        db: 'pubmed',
+        id: ids.join(','),
+        retmode: 'json'
+      }
+    });
+
+    const articles = formatArticles(detailsResponse.data.result);
+
     return {
-      id,
-      title: article.title,
-      abstract: article.abstract || '',
-      authors: article.authors?.map((author: any) => author.name) || [],
-      url: `https://pubmed.ncbi.nlm.nih.gov/${id}`
+      articles,
+      total: parseInt(searchResponse.data.esearchresult.count),
+      query
     };
-  });
+
+  } catch (error) {
+    throw new Error(error.response?.data?.error || error.message);
+  }
+}
+
+function formatArticles(data: any): PubMedArticle[] {
+  return Object.values(data)
+    .filter(article => typeof article === 'object' && article.uid)
+    .map(article => ({
+      id: article.uid,
+      title: article.title,
+      authors: article.authors?.map(author => author.name) || [],
+      abstract: article.abstract || '',
+      journal: article.fulljournalname,
+      publicationDate: article.pubdate,
+      doi: article.elocationid,
+      url: `https://pubmed.ncbi.nlm.nih.gov/${article.uid}/`
+    }));
 }
